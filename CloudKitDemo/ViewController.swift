1d3a347f52aa911557ba30a85eac3edeaef27ae1
Eric Walker <walkereric10@gmail.com>
2019-08-11 09:03:11 -0400

    Initial Commit

---
+++ CloudKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitDemo
+//
+//  Created by Eric Walker on 8/11/19.
+//  Copyright © 2019 Eric Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
